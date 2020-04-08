4f7cc6f0a7cacd3e8be0c10474d99fa7b015c621
LeeChungHyun <38039844+LeeChungHyun@users.noreply.github.com>
2020-04-08 21:45:24 +0900

    Add files via upload

---
+++ File IO/pipe.c
@@ -0,0 +1,33 @@
+#include <direct.h>
+//unix에는 <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+
+int main(){
+    int fd[2];
+    int input, output;
+    int n; 
+    char buf[10];
+
+    pipe(fd);
+    if(fork() == 0){
+        input = open("input.dat",0);
+        close(fd[0]);
+        while((n = read(input, buf, 10)) != 0)
+            write(fd[1], buf, n);
+        close(input);
+        close(fd[1]);
+        exit(0);
+    }
+    else{
+        close(fd[1]);
+        output = open("output.dat", 0666);
+        while((n = read(fd[0], buf, 10)) != 0)
+            write(output, buf, n);
+        close(output);
+        close(fd[0]);
+        wait();
+    }
+}
